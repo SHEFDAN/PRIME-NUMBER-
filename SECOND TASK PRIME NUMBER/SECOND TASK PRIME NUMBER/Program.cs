b912b299372ad04fff15332b1fd93fb47a7de6f5
SHEFDAN <110844468+SHEFDAN@users.noreply.github.com>
2022-08-19 08:33:46 -0700

    Add files via upload

---
+++ SECOND TASK PRIME NUMBER/SECOND TASK PRIME NUMBER/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECOND_TASK_PRIME_NUMBER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isprime = true;
+            Console.WriteLine("Prime Number");
+            for (int i = 2; i <=50; i++)
+            {
+                for (int j = 2; j <=50; j++)
+                {
+                    if (i != j && i % j == 0) 
+                    {
+                        isprime = false;
+                        break;
+
+
+                    }
+
+
+                }
+                if (isprime)
+                { 
+                     Console.WriteLine("\t" + i);
+                }
+                    isprime = true;
+                
+            }
+
+                 Console.ReadKey();
+
+        }
+    }
+}
